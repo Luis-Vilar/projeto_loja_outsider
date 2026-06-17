bf4e6f7897618563070d61809789c9aa85afae21
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-06-17 20:36:49 -0300

    feat : add factory contructor  CamisaModel.fromStringresponse

--- lib/app/data/camisa_model.dart
+++ lib/app/data/camisa_model.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
 abstract class Camisa {
   double precoBase;
   List<String> tamanhosDisponibles;
@@ -16,4 +18,21 @@ final class CamisaModel extends Camisa {
     required super.tamanhosDisponibles,
     required super.modelos,
   });
+
+  factory CamisaModel.fromStringResponse(String response) {
+    final Map<String, dynamic> jsonMap = json.decode(response);
+
+    final double precoBaseResponse = jsonMap['precoBase'] as double;
+    final List<Map<String, String>> modelosResponse =
+        List<Map<String, String>>.from(['modelos']);
+    final List<String> tamanhosDisponiblesResponse = List<String>.from(
+      jsonMap['tamanhosDisponibles'],
+    );
+
+    return CamisaModel(
+      precoBase: precoBaseResponse,
+      modelos: modelosResponse,
+      tamanhosDisponibles: tamanhosDisponiblesResponse,
+    );
+  }
 }
