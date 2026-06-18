3fb9753dada5a08ada07f070c3aef360052333d4
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-06-17 23:46:20 -0300

    style : refactor MyApp to StatefulWidget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,9 +2,14 @@ import 'dart:async';
 import 'dart:developer';
 import 'package:flutter/material.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
