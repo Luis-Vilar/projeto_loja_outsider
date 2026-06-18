d4b2fdc17cf68e959e295f2da21ebd307c960b2b
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2026-06-17 22:18:43 -0300

    style : refactor Camisa and Camisa model

--- lib/app/data/camisa_model.dart
+++ lib/app/data/camisa_model.dart
@@ -3,10 +3,16 @@ import 'dart:convert';
 
 abstract class Camisa {
   double precoBase;
+  double valorPresente;
+  int maxParcelas;
+  double jurosPorParcela;
   List<String> tamanhosDisponibles;
   List<Map<String, dynamic>> modelos;
   Camisa({
     required this.precoBase,
+    required this.valorPresente,
+    required this.maxParcelas,
+    required this.jurosPorParcela,
     required this.tamanhosDisponibles,
     required this.modelos,
   });
@@ -17,6 +23,9 @@ final class CamisaModel extends Camisa {
     required super.precoBase,
     required super.tamanhosDisponibles,
     required super.modelos,
+    required super.valorPresente,
+    required super.maxParcelas,
+    required super.jurosPorParcela,
   });
 
   factory CamisaModel.fromStringResponse(String response) {
@@ -28,11 +37,16 @@ final class CamisaModel extends Camisa {
     final List<String> tamanhosDisponiblesResponse = List<String>.from(
       jsonMap['tamanhosDisponiveis'],
     );
-
+    final double valorPresenteResponse = jsonMap['valorPresente'] as double;
+    final int maxParcelasResponse = jsonMap['maxParcelas'] as int;
+    final double jurosPorParcelaResponse = jsonMap['jurosPorParcela'] as double;
     return CamisaModel(
       precoBase: precoBaseResponse,
       modelos: modelosResponse,
       tamanhosDisponibles: tamanhosDisponiblesResponse,
+      valorPresente: valorPresenteResponse,
+      maxParcelas: maxParcelasResponse,
+      jurosPorParcela: jurosPorParcelaResponse,
     );
   }
 
